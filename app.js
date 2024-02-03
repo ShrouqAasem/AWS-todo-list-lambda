adf68bddc9fa2865698355be0ec0ab53e8dd5ea2
Shrouq Aasem <Eng.Shrouq.Aasem@gmail.com>
2024-02-03 04:44:12 +0200

    AWS-todo-list-app

--- app.js
+++ app.js
@@ -1,1 +1,102 @@
-" " 
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "todo-list";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /todos/{id}":
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /todos/{id}":
+        body = await dynamo.send(
+          new GetCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = body.Item;
+        break;
+      case "GET /todos":
+        body = await dynamo.send(
+          new ScanCommand({ TableName: tableName })
+        );
+        body = body.Items;
+        break;
+      case "POST /todos":
+        const uniqueId = Math.floor(parseInt(Date.now() + "" + Math.random(5) * 10))
+        let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: uniqueId,
+              name: requestJSON.name
+            },
+          })
+        );
+        body = `Post item ${uniqueId}`;
+        break;
+    case "PUT /todos/{id}": 
+        let updateRequestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+            UpdateExpression: "set #name = :name",
+            ExpressionAttributeNames: {
+              "#name": "name",
+            },
+            ExpressionAttributeValues: {
+              ":name": updateRequestJSON.name,
+            },
+          })
+        );
+        body = `Updated item ${event.pathParameters.id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
+
